6c34423ab2e88fbcfb8ea3de12346b2f2de39b88
Josh Asch <josh.asch@gmail.com>
2015-12-02 16:36:05 +0000

    Add some test log statements

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ var http = require('http')
 // Pick the port based on what Azure says should be used.
 var port = process.env.PORT || 1337;
 
+console.log('Starting server on port ' + port);
+
 // Hello, world!
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
+    
+    console.log('Received request');
+
 }).listen(port);
