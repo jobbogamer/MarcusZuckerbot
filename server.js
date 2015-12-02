aacfe350e32010df536690b6f9d2514313926200
Josh Asch <josh.asch@gmail.com>
2015-12-02 19:17:56 +0000

    Basic implementation that can connect to Facebook
    Currently echoes back any message it receives, closes #1

--- server.js
+++ server.js
@@ -2,19 +2,66 @@
 // thing – this component performs the connection to Facebook and collects
 // messages before sending them off to be matched and acted on.
 
-var http = require('http')
+// npm modules
+var login = require('facebook-chat-api');
+var Firebase = require('firebase');
 
 
-// Pick the port based on what Azure says should be used.
-var port = process.env.PORT || 1337;
+// Functions
 
-console.log('Starting server on port ' + port);
+// Start listening to incoming events.
+function startBot(api) {
+    var stopListening = api.listen(function receive(err, event) {
+        if (err) {
+            return console.error(err);
+        }
 
-// Hello, world!
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-    
-    console.log('Received request');
+        if (event.type === 'message') {
+            console.log('Message received in thread ' + event.threadID + ':');
+            console.log(event.body);
 
-}).listen(port);
+            // Simply echo back the message.
+            api.sendMessage(event.body, event.threadID);
+        }
+    });
+    console.log('Zuckerbot is now listening for messages.\n');
+}
+
+
+
+
+// "Main program"
+
+// Print current version of the app.
+var pkg = require('./package.json');
+console.log('Zuckerbot v' + pkg.version + '\n');
+
+// Check that the environment variables are set.
+if (!process.env.FIREBASE) {
+    return console.error('FIREBASE environment variable is not set.');
+}
+if (!process.env.FB_EMAIL) {
+    return console.error('FB_EMAIL environment variable is not set.');
+}
+if (!process.env.FB_PASSWORD) {
+    return console.error('FB_PASSWORD environment variable is not set.');
+}
+
+// Connect to the Firebase database.
+console.log('Connecting to Firebase...');
+var db = new Firebase(process.env.FIREBASE);
+console.log('Done.\n');
+
+// Connect to Facebook chat.
+console.log('Connecting to Facebook chat...');
+login({
+    email: process.env.FB_EMAIL,
+    password: process.env.FB_PASSWORD
+}, function loggedIn(err, api) {
+    if (err) {
+        return console.error(err);
+    }
+
+    console.log('Done.\n');
+    startBot(api);
+});
