072113dce26e9ccbdd5f00bd9358665a86a31576
Josh Asch <josh.asch@gmail.com>
2015-12-03 17:37:00 +0000

    Implement help()
    Closes #2, closes #3, closes #4, and closes #5

--- messageHandler.js
+++ messageHandler.js
@@ -5,29 +5,107 @@
 // List of commands which can be called.
 var commands = {};
 
+// Usage string for each command.
+var docstrings = {};
+
 
 
 
 
 // Commands
 
-// Return a list of available commands.
-commands.help = function() {
-    var list = '';
+commands.help = function(arguments) {
+    if (arguments.length == 0) {
+        // Create a list of all commands.
+        var list = '';
 
-    for (var command in commands) {
-        list += '• ' + command + '\n';
+        // Just add each command to the end of the list.
+        for (var command in commands) {
+            list += '• ' + command + '\n';
+        }
+
+        // Add some explanation to the message.
+        var messageBody = 'Available commands:\n';
+        messageBody += list;
+
+        // Build and return the reply.
+        var reply = {
+            body: messageBody
+        };
+        return reply;
+    }
+    else {
+        // Find the usage instructions for the named command.
+        var commandName = arguments[0];
+
+        // Return help for the command, if it exists.
+        if (docstrings[commandName]) {
+            var messageBody = 'Help for ' + commandName + ':';
+            for (var i = 0; i < docstrings[commandName].usage.length; i++) {
+                messageBody += '\n\n';
+                messageBody += docstrings[commandName].usage[i] + '\n';
+                messageBody += docstrings[commandName].details[i];
+            }
+
+            var reply = {
+                body: messageBody
+            };
+            return reply;
+        }
+        
+        // No command matching the argument.
+        var messageBody = 'No command called \'' + commandName + '\'.';
+        var reply = {
+            body: messageBody
+        };
+        return reply;
+    }
+};
+docstrings.help = {};
+docstrings.help.usage = [
+    'help()',
+    'help(command)'
+];
+docstrings.help.details = [
+    'Display a list of available commands.',
+    'Display usage instructions for the given command.'
+];
+
+
+
+
+
+// Extract the command name and arguments from a message.
+function parse(message) {
+    // Find the name of the command.
+    var commandStart = message.indexOf('zb.') + 3;
+    var commandEnd = message.indexOf('(');
+    var commandLength = commandEnd - commandStart;
+    var commandName = message.substr(commandStart, commandLength);
+
+    // Create a regular expression which can match arguments. String arguments
+    // must be enclosed in quotes, but numerical arguments do not.
+    var argRegex = /['‘](.+?)['’]|([0-9.]+)/g;
+
+    // Match any arguments passed in.
+    var argStart = commandEnd + 1;
+    var argEnd = message.indexOf(')');
+    var argLength = argEnd - argStart;
+    var match = message.substr(argStart, argLength).match(argRegex) || [];
+
+    for (var i = 0; i < match.length; i++) {
+        if (match[i].indexOf('\'') !== -1 || match[i].indexOf('‘') !== -1) {
+            match[i] = match[i].substr(1, match[i].length - 2);
+        }
     }
 
-    var messageBody = 'Available commands:\n';
-    messageBody += list;
-
-    var reply = {
-        body: messageBody
+    return {
+        command: commandName,
+        arguments: match
     };
-
-    return reply;
 }
+
+
 
 
 
@@ -38,22 +116,22 @@ var handle = function(message, chat) {
     var body = message.trim();
 
     // Ignore any messages which don't start with 'zb.' as they aren't commands.
-    if (body.indexOf('zb.') !== 0 || body.indexOf('(') === -1) {
+    if (body.indexOf('zb.') !== 0 || body.indexOf('(') === -1 || body.indexOf(')') === -1) {
         console.log('Message does not contain a command.');
         return null;
     }
 
-    // Find the name of the command.
-    var commandStart = body.indexOf('zb.') + 3;
-    var commandEnd = body.indexOf('(');
-    var commandLength = commandEnd - commandStart;
-    var commandName = body.substr(commandStart, commandLength);
+    // Parse the command name and arguments.
+    var parsed = parse(body);
+    var commandName = parsed.command;
+    var arguments = parsed.arguments;
     console.log('Command name is \'' + commandName + '\'.');
+    console.log('Arguments are ' + arguments.join(', ') + '.');
 
     // If the given command matches an existing command, call it.
     if (commands[commandName]) {
         console.log('Matched command ' + commandName + '.');
-        return commands[commandName]();
+        return commands[commandName](arguments);
     }
 
     // A command wasn't matched
@@ -62,5 +140,5 @@ var handle = function(message, chat) {
 }
 
 
-
+// Only export the handle function, everything else is private.
 module.exports.handle = handle;
