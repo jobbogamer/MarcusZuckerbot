36d6eff66826455239b33ac5370c55bb903c9aa9
Josh Asch <josh.asch@gmail.com>
2015-12-06 18:35:10 +0000

    Port getValue() to the new architecture

---
+++ commands/getValue.js
@@ -0,0 +1,33 @@
+// Display the value of a variable.
+
+var getValue = function(arguments, info, replyCallback) {
+    var reply = '';
+
+    var value = info.chatData.variables[arguments.variable];
+    if (value != null) {
+        reply = arguments.variable + ' is currently set to ' + value + '.';
+    }
+    else {
+        reply = arguments.variable + ' is not defined.';
+    }
+
+    replyCallback({
+        body: reply
+    });
+}
+
+var usage = [
+    {
+        arguments: ['variable'],
+        description: 'Display the current value of the given variable.'
+    }
+];
+
+
+module.exports = function init() {
+    return {
+        name: 'getValue',
+        func: getValue,
+        usage: usage
+    }
+};
