93d52bff655748964192612c47b50fe32d398304
Josh Asch <josh.asch@gmail.com>
2015-12-07 22:54:34 +0000

    Add multiline support to diagonalise()
    This allows messages with more than 19 characters to be diagonalised.
    
    Closes #46

--- commands/diagonalise.js
+++ commands/diagonalise.js
@@ -3,26 +3,85 @@
 
 var diagonalise = function(arguments, info, replyCallback) {
     var reply = '';
-
+    var argc = Object.keys(arguments).length;
     var text = arguments.text.toUpperCase();
 
-    // Text cannot be longer than 19 characters long.
-    if (text.length > 19) {
-        reply = 'The text is too long to diagonalise.';
+    // Argument 2 must be a boolean.
+    if (argc == 2 && arguments.multiline !== 'true' && arguments.multiline !== 'false') {
+        reply = 'Error: Argument 2 of diagonalise() must be a boolean.';
     }
     else {
-        for (var i = 0; i < text.length; i++) {
-            var character = text.charAt(i);
-            for (var j = 0; j < i; j++) {
-                reply += '   ';
+        // Use the simple one-line version if multiline is specifically turned
+        // off, or if the text is short enough to fit on one line anyway.
+        if ((argc == 2 && arguments.multiline === 'false') || arguments.text.length <= 19) {
+            // Text cannot be longer than 19 characters long.
+            if (text.length > 19) {
+                reply = 'The text is too long to diagonalise.';
             }
+            else {
+                for (var i = 0; i < text.length; i++) {
+                    var character = text.charAt(i);
+                    for (var j = 0; j < i; j++) {
+                        reply += '   ';
+                    }
 
-            if (character === 'I') {
-                reply += ' ';
+                    if (character === 'I') {
+                        reply += ' ';
+                    }
+
+                    reply += character;
+                    reply += '\n';
+                }
             }
+        }
 
-            reply += character;
-            reply += '\n';
+        // If multiline is true, be more clever about it and create multiple lines.
+        else {
+            // Go word by word, and if the next word would push the current line
+            // over the 19 character limit, start a new word.
+            var length = 0;
+            var words = text.split(' ');
+
+            for (var i = 0; i < words.length; i++) {
+                var word = words[i];
+
+                // Still can't diagonalise if one of the words is over 19
+                // characters long. (Supercalifragilisticexpialidocius)
+                if (word.length > 19) {
+                    reply = 'Word ' + (i + 1) + ' is too long to diagonalise.';
+                    break;
+                }
+
+                // If the word doesn't fit on the current line, start a new line.
+                if (length + word.length > 19) {
+                    length = 0;
+                    reply += '\n';
+                }
+
+                // Add each character of the word.
+                for (var j = 0; j < word.length; j++) {
+                    var character = word.charAt(j);
+                    // Indent.
+                    for (var k = 0; k < length + j; k++) {
+                        reply += '   ';
+                    }
+
+                    if (character === 'I') {
+                        reply += ' ';
+                    }
+
+                    reply += character;
+                    reply += '\n';
+                }
+
+                length += word.length;
+
+                // Add the space for the end of the word.
+                if (i < words.length - 1) {
+                    reply += '\n';
+                    length += 1;
+                }
+            }
         }
     }
 
@@ -36,6 +95,12 @@ var usage = [
         arguments: ['text'],
         description: 'Resend a message with one character on each line, spaced to create a ' +
                      'diagonal line effect.'
+    },
+    {
+        arguments: ['text', 'multiline'],
+        description: 'Resend a message with one character on each line, spaced to create a ' +
+                     'diagonal line effect. Set multiline to false to stop the message from ' +
+                     'being split into multiple lines.'
     }
 ];
 
