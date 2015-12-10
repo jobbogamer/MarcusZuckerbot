40992c3c88f1647152dde56e126ebacc65ce8f36
Josh Asch <josh.asch@gmail.com>
2015-12-10 14:38:55 +0000

    Allow deployment notifications to be turned on and off

---
+++ commands/setNotificationsEnabled.js
@@ -0,0 +1,49 @@
+// Turn deployment notifications on or off for the current conversation.
+
+var setNotificationsEnabled = function(arguments, info, replyCallback) {
+    // Default to an empty object if this is a new chat.
+    var chatData = info.chatData || {};
+    var reply = '';
+
+    if (arguments.setting === 'true') {
+        if (chatData.notifications) {
+            reply = 'Deployment notifications are already on for this conversation.'
+        }
+        else {
+            chatData.notifications = true;
+            reply = 'Deployment notifications have been turned on for this conversation.'
+        }
+    }
+    else {
+        if (!chatData.notifications) {
+            reply = 'Deployment notifications are already off for this conversation.'
+        }
+        else {
+            chatData.notifications = false;
+            reply = 'Deployment notifications have been turned off for this conversation.'
+        }
+    }
+
+    var message = {
+        body: reply
+    };
+
+    replyCallback(message, chatData);
+}
+
+var usage = [
+    {
+        arguments: ['setting'],
+        description: 'Turn deployment notifications on or off. Set setting to true ' + 
+                     'to turn them on, or false to turn them off.'
+    }
+];
+
+
+module.exports = function init() {
+    return {
+        name: 'setNotificationsEnabled',
+        func: setNotificationsEnabled,
+        usage: usage
+    }
+};
---
+++ test/setNotificationsEnabled.js
@@ -0,0 +1,117 @@
+
+var init = require('../commands/setNotificationsEnabled');
+var command = init();
+
+describe('setNotificationsEnabled', function() {
+    describe('execute', function() {
+        it('should turn on notifications when the argument is true', function(done) {
+            // Set the 'setting' argument to true.
+            var arguments = {
+                setting: 'true'
+            };
+
+            // Create a chat info object where notifications are turned off.
+            var chatData = {
+                notifications: false
+            };
+
+            // Put the chatData into an info object.
+            var info = {
+                chatData: chatData
+            };
+
+            command.func(arguments, info, function replyCallback(reply, chat) {
+                // The command should reply with a message saying that
+                // notifications have been enabled.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+
+                // The reply should be helpful. It should say something about
+                // 'notifications' being turned 'on' or 'enabled'.
+                reply.body.should.match(/notifications/gi);
+                reply.body.should.match(/(on)|(enabled)/gi);
+
+                // Notifications should have actually been enabled.
+                chat.should.be.Object();
+                chat.should.have.property('notifications');
+                chat.notifications.should.be.true();
+
+                done();
+            });
+        });
+
+
+        it('should turn off notifications when the argument is false', function(done) {
+            // Set the 'setting' argument to false.
+            var arguments = {
+                setting: 'false'
+            };
+
+            // Create a chat info object where notifications are turned on.
+            var chatData = {
+                notifications: true
+            };
+
+            // Put the chatData into an info object.
+            var info = {
+                chatData: chatData
+            };
+
+            command.func(arguments, info, function replyCallback(reply, chat) {
+                // The command should reply with a message saying that
+                // notifications have been disabled.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+
+                // The reply should be helpful. It should say something about
+                // 'notifications' being turned 'off' or 'disabled'.
+                reply.body.should.match(/notifications/gi);
+                reply.body.should.match(/(off)|(disabled)/gi);
+
+                // Notifications should have actually been disabled.
+                chat.should.Object();
+                chat.should.have.property('notifications');
+                chat.notifications.should.be.false();
+
+                done();
+            });
+        });
+
+        
+        it('should not fail if notifications are already turned on', function(done) {
+            // Set the 'setting' argument to true.
+            var arguments = {
+                setting: 'true'
+            };
+
+            // Create a chat info object where notifications are turned on.
+            var chatData = {
+                notifications: true
+            };
+
+            // Put the chatData into an info object.
+            var info = {
+                chatData: chatData
+            };
+
+            command.func(arguments, info, function replyCallback(reply, chat) {
+                // The command should reply with a message saying that
+                // notifications are enabled.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+
+                // The reply should be helpful. It should say something about
+                // 'notifications' being turned 'on' or 'enabled'.
+                reply.body.should.match(/notifications/gi);
+                reply.body.should.match(/(on)|(enabled)/gi);
+
+                // Notifications should still be enabled.
+                chat.should.Object();
+                chat.should.have.property('notifications');
+                chat.notifications.should.be.true();
+
+                done();
+            });
+        });
+    });
+});
