a8e4612e9836900df3bdb6af26f29a9b3f6cf6b3
Josh Asch <josh.asch@gmail.com>
2015-12-10 11:47:34 +0000

    Use an EVEN better started message

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function startBot(api, chats) {
 
     // Notify subscribed chats that Zuckerbot is running.
     var message = {
-        body: 'Zuckerbot has started, running v' + pkg.version + '.'
+        body: 'Zuckerbot is now running v' + pkg.version + '.'
     };
 
     Object.keys(chats).forEach(function(key) {
