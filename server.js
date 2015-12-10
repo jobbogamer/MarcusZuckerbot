80cb466b948d48f0aa24cf8ca8cf5e62dbe9d9dc
Josh Asch <josh.asch@gmail.com>
2015-12-10 00:20:04 +0000

    Get rid of app.set()

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080);
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(port, ip, function() {
+    console.log('Express server listening on port ' + port);
 });
 
 app.get('/', function (req, res) {
