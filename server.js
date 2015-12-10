17ca3886298196f6a2326d5b99a4f1b46cc1e9b5
Josh Asch <josh.asch@gmail.com>
2015-12-10 00:25:38 +0000

    Try using 3002 as the default port

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
-http.createServer(app).listen(port, ip, function() {
-    console.log('Express server listening on port ' + port);
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
 
 app.get('/', function (req, res) {
