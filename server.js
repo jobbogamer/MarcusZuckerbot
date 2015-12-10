006478c9828e42547dee67a8cab30bbe476a26a8
Josh Asch <josh.asch@gmail.com>
2015-12-10 11:30:27 +0000

    Remove express log statement

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    // The server is listening.
 });
 
 app.get('/', function (req, res) {
