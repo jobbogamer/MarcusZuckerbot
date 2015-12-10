6afcb305ee9605324cafe9b45bb4c26a2313ae9b
Josh Asch <josh.asch@gmail.com>
2015-12-10 23:30:08 +0000

    Test getValue()

---
+++ test/test_getValue.js
@@ -0,0 +1,57 @@
+
+var init = require('../commands/getValue');
+var command = init();
+
+describe('getValue', function() {
+    describe('execute', function() {
+        it('should display the value of a variable that exists', function(done) {
+            var arguments = {
+                variable: 'nuggets'
+            };
+
+            var info = {
+                chatData: {
+                    variables: {
+                        nuggets: 5
+                    }
+                }
+            };
+
+            command.func(arguments, info, function replyCallback(reply, chat) {
+                // The reply should display the variable name and the value.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+                reply.body.should.be.String();
+                reply.body.should.match(/nuggets/gi);
+                reply.body.should.match(/5/g);
+
+                done();
+            });
+        });
+
+
+        it('should cope when the variable does not exist', function(done) {
+            var arguments = {
+                variable: 'sausage'
+            };
+
+            var info = {
+                chatData: {
+                    variables: {
+                        nuggets: 5
+                    }
+                }
+            };
+
+            command.func(arguments, info, function replyCallback(reply, chat) {
+                // The reply should display the variable name and the value.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+                reply.body.should.be.String();
+                reply.body.should.match(/(undefined)|(not defined)|(not exist)/gi);
+
+                done();
+            });
+        });
+    });
+});
