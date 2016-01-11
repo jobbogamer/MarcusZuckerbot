60285e3053cf724318d88b68ca08f0656d9708f1
Josh Asch <josh.asch@gmail.com>
2016-01-11 23:44:01 +0000

    Display progress variables in showVariables()
    Closes #70

--- commands/showVariables.js
+++ commands/showVariables.js
@@ -1,24 +1,44 @@
 // List all defined variables for the current conversation.
 
+var helpers = require('../helpers');
+
 var showVariables = function(arguments, info, replyCallback) {
     var reply = '';
 
     // Default to an empty object in case no variables are defined.
     var variables = info.chatData.variables || {};
+    var progressVariables = info.chatData.progress || {};
 
+    var variableList = '';
+    var progressList = '';
+
+    // Iterate over all variables.
     var keys = Object.keys(variables);
     keys.forEach(function(key) {
         if (variables[key] != null) {
-            reply += key + ' = ' + variables[key] + '\n';
+            variableList += key + ' = ' + variables[key] + '\n';
         }
     });
 
-    if (reply.length == 0) {
-        reply = 'No variables are defined.';
+    // Iterate over all progress variables.
+    var keys = Object.keys(progressVariables);
+    keys.forEach(function(key) {
+        if (progressVariables[key] != null) {
+            progressList += key + ' = ' + helpers.round(progressVariables[key]) + '\n';
+        }
+    });
+
+    // Check that variables are actually defined, by seeing if any were added to
+    // the lists.
+    if (variableList.length == 0) {
+        variableList = 'No variables are defined.';
+    }
+    if (progressList.length == 0) {
+        progressList = 'No progress variables are defined.';
     }
 
     replyCallback({
-        body: reply
+        body: 'Variables:\n' + variableList + '\nProgress Variables:\n' + progressList
     });
 }
 
--- test/test_showVariables.js
+++ test/test_showVariables.js
@@ -1,5 +1,6 @@
 
 var init = require('../commands/showVariables');
+var should = require('should');
 var command = init();
 
 describe('showVariables', function() {
@@ -32,8 +33,8 @@ describe('showVariables', function() {
         it('should display a message if no variables are defined', function(done) {
             var info = {
                 chatData: {
-                    variables: {
-                    }
+                    variables: {},
+                    progress: {}
                 }
             };
 
@@ -43,6 +44,61 @@ describe('showVariables', function() {
                 reply.should.have.property('body');
                 reply.body.should.be.String();
                 reply.body.should.match(/no variables/gi);
+
+                done();
+            });
+        });
+
+
+        it('should list progress variables', function(done) {
+            var info = {
+                chatData: {
+                    variables: {},
+                    progress: {
+                        'nuggets': 8,
+                        'sausage': 4,
+                        'biscuits': 11.526666  // Check numbers are rounded;
+                                               // if they are not, 11.526 won't
+                                               // match 11.527 in the regex.
+                    }
+                }
+            };
+
+            command.func({}, info, function replyCallback(reply, chat) {
+                // The reply should list progress variables.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+                reply.body.should.be.String();
+                reply.body.should.match(/nuggets\s*(:|=)\s*8/gi);
+                reply.body.should.match(/sausage\s*(:|=)\s*4/gi);
+                reply.body.should.match(/biscuits\s*(:|=)\s*11.527/gi);
+
+                done();
+            });
+        });
+
+
+        it('should list both regular and progress variables', function(done){
+            var info = {
+                chatData: {
+                    variables: {
+                        'biscuits': 11
+                    },
+                    progress: {
+                        'nuggets': 8,
+                        'sausage': 4
+                    }
+                }
+            };
+
+            command.func({}, info, function replyCallback(reply, chat) {
+                // The reply should list progress variables.
+                reply.should.be.Object();
+                reply.should.have.property('body');
+                reply.body.should.be.String();
+                reply.body.should.match(/nuggets\s*(:|=)\s*8/gi);
+                reply.body.should.match(/sausage\s*(:|=)\s*4/gi);
+                reply.body.should.match(/biscuits\s*(:|=)\s*11/gi);
 
                 done();
             });
