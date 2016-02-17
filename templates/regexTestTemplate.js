340b72c818dfc99d1dd44306c007c5b2bc2c0d5a
Josh Asch <josh.asch@gmail.com>
2016-02-17 16:12:06 +0000

    Update createPlugin.js and create createRegexPlugin.js

--- templates/regexTestTemplate.js
+++ templates/regexTestTemplate.js
@@ -1,18 +1,23 @@
 
-var init = require('../../commands/COMMAND_NAME');
+var init = require('../../regexCommands/COMMAND_NAME');
 var should = require('should');
 
 
 describe('COMMAND_NAME', function() {
+    describe('match', function() {
+        // Test that the regex only matches expected messages
+    });
+
+
     describe('execute', function() {
         it('should do something', function(done) {
             var command = init();
 
-            var arguments = {};
+            var matches = [];
 
             var info = {};
 
-            command.func(arguments, info, function replyCallback(reply, chat) {
+            command.func(matches, info, function replyCallback(reply, chat) {
                 reply.should.be.Object();
                 reply.should.have.property('body');
                 reply.body.should.be.String();
